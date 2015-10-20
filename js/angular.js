de3263e48da7c790ec49d21f57eb483e68fe6d9f
S. M. Abrar Jahin <abrarjahin@live.com>
2015-10-20 15:45:22 +0600

    Done, common angular.js example done

--- js/angular.js
+++ js/angular.js
@@ -1,9 +1,89 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope)	//$scope = whole model
+////////////////////////////////////////////////////////////////////////index.html
+angular.module('myApp', []).controller('myCtrl', function($scope)	//$scope = whole model
 {
 	$scope.init = function()
 	{
 		$scope.firstName	= 'John';
 		$scope.lastName		= 'Doe';
 	}
+
+	$scope.names	=	[
+							{name:'Jani',country:'Norway'},
+							{name:'Hege',country:'Sweden'},
+							{name:'Kai',country:'Denmark'}
+						];
+
+	$scope.fullName = function()
+	{
+        return $scope.firstName + " " + $scope.lastName;
+    }
+});
+
+//////////////////////////////////////////////////////////////////////////SQL.html
+angular.module('sql', []).controller('customersCtrl', function($scope, $http)
+{
+	$http.get("http://www.w3schools.com/angular/customers_mysql.php")
+		.success(function (response)
+		{
+			$scope.names = response.records;
+		});
+});
+
+//////////////////////////////////////////////////////////////////////////validation.html
+angular.module('validation', []).controller('validateCtrl', function($scope)
+{
+    $scope.user = 'John Doe';
+    $scope.email = 'john.doe@gmail.com';
+});
+
+////////////////////////////////////////////////////////////////////////////bootstrap.html
+angular.module('myApp', []).controller('userCtrl', function($scope) {
+$scope.fName = '';
+$scope.lName = '';
+$scope.passw1 = '';
+$scope.passw2 = '';
+$scope.users = [
+{id:1, fName:'Hege', lName:"Pege" },
+{id:2, fName:'Kim',  lName:"Pim" },
+{id:3, fName:'Sal',  lName:"Smith" },
+{id:4, fName:'Jack', lName:"Jones" },
+{id:5, fName:'John', lName:"Doe" },
+{id:6, fName:'Peter',lName:"Pan" }
+];
+$scope.edit = true;
+$scope.error = false;
+$scope.incomplete = false; 
+
+$scope.editUser = function(id) {
+  if (id == 'new') {
+    $scope.edit = true;
+    $scope.incomplete = true;
+    $scope.fName = '';
+    $scope.lName = '';
+    } else {
+    $scope.edit = false;
+    $scope.fName = $scope.users[id-1].fName;
+    $scope.lName = $scope.users[id-1].lName; 
+  }
+};
+
+$scope.$watch('passw1',function() {$scope.test();});
+$scope.$watch('passw2',function() {$scope.test();});
+$scope.$watch('fName', function() {$scope.test();});
+$scope.$watch('lName', function() {$scope.test();});
+
+$scope.test = function() {
+  if ($scope.passw1 !== $scope.passw2) {
+    $scope.error = true;
+    } else {
+    $scope.error = false;
+  }
+  $scope.incomplete = false;
+  if ($scope.edit && (!$scope.fName.length ||
+  !$scope.lName.length ||
+  !$scope.passw1.length || !$scope.passw2.length)) {
+     $scope.incomplete = true;
+  }
+};
+
 });
